832bda9386dad42d25aa5e35b7857a86468bec32
botirk <14036046+botirk@users.noreply.github.com>
2025-06-13 13:22:47 +0300

    sessions done

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,27 +1,34 @@
 import i18next from "i18next"
 import { paths } from "./index.js"
+import fastifyPassport from '@fastify/passport'
 
 export default (app) => {
     app.get(paths.login(), (_, res) => {
         res.render('login.pug')
     })
 
-    app.post(
-        paths.session(),
-        { preValidation: app.passport.authenticate('form') },
-        async (req, res) => {
-            if (req.body._method === 'delete') {
-                await req.logOut()
-                res.flash('info', i18next.t('layout.logout'))
-                return res.redirect(paths.main())
-            }
-            if (!req.user) {
-                req.flash('warning', i18next.t('login.error'))
-                return res.code(401).render('login', { user: req.body.data })
-            }
-            await req.logIn(req.user)
-            req.flash('success', i18next.t('login.success'))
+    fastifyPassport.strategy()
+
+    app.post(paths.session(), async (req, res) => {
+        if (req.body._method === 'delete') {
+            await req.logOut()
+            res.flash('info', i18next.t('layout.logout'))
+            return res.redirect(paths.main())
+        } else if (req.user) {
             return res.redirect(paths.main())
         }
-    )
+
+        return await new Promise(async (resolve) => {
+            app.passport.authenticate('form', async (_1, _2, _3, user) => {
+                if (!user) {
+                    req.flash('warning', i18next.t('login.error'))
+                    resolve(res.code(401).render('login', { user: req.body.data }))
+                } else {
+                    await req.logIn(user)
+                    req.flash('success', i18next.t('login.success'))
+                    resolve(res.redirect(paths.main()))
+                }
+            })(req, res)
+        })
+    })
 }
